73afc0e66771e54c24307c9b7af45faa182e6c75
BrunoScmitz4 <brunosch250@gmail.com>
2024-09-04 13:33:32 -0300

    Update

---
+++ main.py
@@ -0,0 +1,24 @@
+poder = ""
+cabelo_cor = ""
+roupa_p = ""
+nome_p  = ""
+hp_p = 0
+atk_p = 0
+mana = 0
+opcao_classe = ""
+# Tela de Login
+
+opcao_func = 0 # Opção Funcionalidade
+
+while(opcao_func != 100):
+    while(nome_p == ""):
+        print("|====================================|")
+        print("|  Bem vindo(a) à Reino de Etéria!   |")
+        print("|____________________________________|")
+        nome_p = str(input("Nome do usuário: "))
+
+        print("Personalize seu personagem: ")
+        print("Qual será a cor do seu cabelo: ")
+        cabelo_cor = str(input("Cor: "))
+        roupa_p = str(input("Descreva a roupa do personagem: "))
+    opcao_func = int(input("[1] - Iniciar/Continuar Jogo\n[2] - Configurações\n[100] - Sair",))
